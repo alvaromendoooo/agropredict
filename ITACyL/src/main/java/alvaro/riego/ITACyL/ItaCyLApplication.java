9d9bfa565140b1e4c69f7689a4a408ad7ee8003f
alvaromendoooo <alvaromendo2004@gmail.com>
2025-12-30 11:25:55 +0100

    chore: Initialization of ITACyL service

---
+++ ITACyL/src/main/java/alvaro/riego/ITACyL/ItaCyLApplication.java
@@ -0,0 +1,13 @@
+package alvaro.riego.ITACyL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaCyLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaCyLApplication.class, args);
+	}
+
+}
